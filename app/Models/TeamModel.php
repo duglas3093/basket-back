fecfb4b973e2111a81cfcc00d9422a1ac591259d
Douglas Quisbert <douglasquisbert.m30@gmail.com>
2022-11-08 19:50:49 -0400

    08.11.22 added team

--- app/Models/TeamModel.php
+++ app/Models/TeamModel.php
@@ -8,17 +8,15 @@ class TeamModel extends Model
 {
     protected $DBGroup          = 'default';
     protected $table            = 'equipo';
-    protected $primaryKey       = [
-        'id',
-        'usuario_id',
-        'campeonato_id'
-    ];
+    protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
     protected $allowedFields    = [
+        'usuario_id',
+        'campeonato_id',
         'nro_jugadores',
         'nombre',
         'categoria',
@@ -39,8 +37,80 @@ class TeamModel extends Model
     protected $deletedField  = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
+    protected $validationRules      = [
+        'usuario_id'=>[
+            'label'=>'usuario_id',
+            'rules'=> 'required'
+        ],
+        'campeonato_id'=>[
+            'label'=>'campeonato_id',
+            'rules'=> 'required'
+        ],
+        'nro_jugadores'=>[
+            'label'=>'nro_jugadores',
+            'rules'=> 'required'
+        ],
+        'nombre'=>[
+            'label'=>'nombre',
+            'rules'=> 'required'
+        ],
+        'categoria'=>[
+            'label'=>'categoria',
+            'rules'=> 'required'
+        ],
+        'entrenador'=>[
+            'label'=>'entrenador',
+            'rules'=> 'required'
+        ],
+        'habilitado'=>[
+            'label'=>'habilitado',
+            'rules'=> 'required'
+        ],
+        'pais'=>[
+            'label'=>'pais',
+            'rules'=> 'required'
+        ],
+        'puntos_acum'=>[
+            'label'=>'puntos_acum',
+            'rules'=> 'required'
+        ],
+        'fyh_inscripcion'=>[
+            'label'=>'fyh_inscripcion',
+            'rules'=> 'required'
+        ],
+    ];
+    protected $validationMessages   = [
+        'usuario_id' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'campeonato_id' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'nro_jugadores' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'nombre' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'categoria' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'entrenador' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'habilitado' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'pais' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'puntos_acum' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'fyh_inscripcion' => [
+            'required'  => 'El campo {field} es requerido.'
+        ]
+    ];
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
 
