561a4f7d6ab992a787081e025dc2a5c553c70d3b
Douglas Quisbert <douglasquisbert.m30@gmail.com>
2022-11-08 20:07:00 -0400

    08.11.22 added player

--- app/Models/PlayerModel.php
+++ app/Models/PlayerModel.php
@@ -8,12 +8,7 @@ class PlayerModel extends Model
 {
     protected $DBGroup          = 'default';
     protected $table            = 'jugador';
-    protected $primaryKey       = [
-                                    'usuario_id',
-                                    'equipo_campeonato_id',
-                                    'equipo_usuario_id',
-                                    'equipo_id',
-    ];
+    protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
     protected $returnType       = 'array';
@@ -37,8 +32,59 @@ class PlayerModel extends Model
     protected $deletedField  = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
+    protected $validationRules      = [
+        'usuario_id'=>[
+            'label'=>'usuario_id',
+            'rules'=> 'required'
+        ],
+        'equipo_campeonato_id'=>[
+            'label'=>'equipo_campeonato_id',
+            'rules'=> 'required'
+        ],
+        'equipo_usuario_id'=>[
+            'label'=>'equipo_usuario_id',
+            'rules'=> 'required'
+        ],
+        'equipo_id'=>[
+            'label'=>'equipo_id',
+            'rules'=> 'required'
+        ],
+        'estatura'=>[
+            'label'=>'estatura',
+            'rules'=> 'required'
+        ],
+        'peso'=>[
+            'label'=>'peso',
+            'rules'=> 'required'
+        ],
+        'dorsal'=>[
+            'label'=>'dorsal',
+            'rules'=> 'required'
+        ],
+    ];
+    protected $validationMessages   = [
+        'usuario_id' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'equipo_campeonato_id' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'equipo_usuario_id' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'equipo_id' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'estatura' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'peso' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+        'dorsal' => [
+            'required'  => 'El campo {field} es requerido.'
+        ],
+    ];
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
 
